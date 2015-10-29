b3d6fc4757e20721239a93da1dd23a389c6415ff
茗香墨语 <yaowen424@126.com>
2015-10-29 17:17:46 +0800

    新增了前台服务运行代码

--- app/src/main/java/com/yaowen/androidservice/MyService.java
+++ app/src/main/java/com/yaowen/androidservice/MyService.java
@@ -1,5 +1,7 @@
 package com.yaowen.androidservice;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -24,6 +26,20 @@ public class MyService extends Service {
     public void onCreate() {
         Log.d("TAG", "MyService onCreate");
         super.onCreate();
+
+        Intent notificationIntent = new Intent(this, ServiceActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        //Notification notification = new Notification(R.mipmap.ic_launcher, "这个一个前台服务！", System.currentTimeMillis());
+        //notification.setLatestEventInfo(this, "This is title", "This is content", pendingIntent);//该方法已经过时，不可使用了
+        Notification.Builder builder = new Notification.Builder(MyService.this);
+        builder.setContentIntent(pendingIntent).setSmallIcon(R.mipmap.ic_launcher)
+                .setWhen(System.currentTimeMillis())//设置时间发生时间
+                .setAutoCancel(true)//设置可以清除
+                .setContentTitle("this is the ContentTitle!")//设置下拉列表里的标题
+                .setContentText("this is ContentText!");//设置上下文内容
+        Notification notification = builder.getNotification();
+        startForeground(1, notification);
     }
 
     @Override
